df2c921de5f1c8b09cc41239fdb398307a67b580
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-23 16:32:57 -0600

    Move App to it's own file add components to render
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

---
+++ src/Containers/App/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PaletteArea from '../PaletteArea/PaletteArea';
+import Sidebar from '../Sidebar/Sidebar';
+import Error from '../../Components/Error/Error';
+import Loading from '../../Components/Loading/Loading';
+import PaletteForm from '../PaletteForm/PaletteForm';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Palette Picker</h1>
+        <button>Generate A Palette</button>
+        <PaletteArea/>
+        <PaletteForm/>
+        <Sidebar/>
+        <Error/>
+        <Loading/>
+      </div>
+    );
+  }
+}
+
+export default App;
