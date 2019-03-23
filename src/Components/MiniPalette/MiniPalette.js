767c1012afe444b03c6affbbc3b54148a803e220
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-23 16:36:10 -0600

    Add MiniPaletteArea and MiniPalette components
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

---
+++ src/Components/MiniPalette/MiniPalette.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MiniPalette = () => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default MiniPalette;
---
+++ src/Containers/MiniPaletteArea/MiniPaletteArea.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MiniPalette from '../../Components/MiniPalette/MiniPalette';
+
+export default class MiniPaletteArea extends Component{
+  render() {
+    return(
+      <div>
+        <MiniPalette/>
+      </div>
+    )
+  }
+}
