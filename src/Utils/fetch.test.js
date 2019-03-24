f0d5dead8f5c6dfeadfa7354876529911f0a87e4
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-24 16:43:43 -0600

    Add tests for fetchData and createOptions
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

---
+++ src/Utils/fetch.test.js
@@ -0,0 +1,42 @@
+import { fetchData, createOptions } from './fetch';
+import * as mockData from '../mockData';
+
+describe('Utils', () => {
+  describe('fetchData', () => {
+    const mockPath = '/projects';
+    
+    it('should return projects if status is less than 300', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: jest.fn(() => Promise.resolve(mockData.mockProjects)),
+        status: 200
+      }));
+      const result = await fetchData(mockPath);
+      expect(result).toEqual(mockData.mockProjects);
+    });
+
+    it('should throw an error if status is >= 300', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: jest.fn(() => Promise.resolve('Error fetching projects')),
+        status: 500
+      }));
+      const expected = new Error('Error fetching projects');
+      await expect(fetchData(mockPath)).rejects.toEqual(expected);
+    });
+  });
+
+  describe('createOptions', () => {
+    it('should return an object with method, body and headers', () => {
+      const mockMethod = 'POST';
+      const mockData = { name: 'Awesome Project' };
+      const expected = {
+        method: mockMethod,
+        body: JSON.stringify(mockData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      const result = createOptions(mockMethod, mockData);
+      expect(result).toEqual(expected);
+    });
+  });
+});
