229709b29d0cb9aea1460ed18f74348b8d2279ad
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-24 14:16:00 -0600

    Update rootReducer
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { combineReducers } from 'redux';
-import currentPaletteReducer from './CurrentPaletteReducer';
-import errorReducer from './ErrorReducer';
-import loadingReducer from './LoadingReducer';
-import projectsReducer from './ProjectsReducer';
+import { currentPaletteReducer } from './CurrentPaletteReducer';
+import { errorReducer } from './ErrorReducer';
+import { loadingReducer } from './LoadingReducer';
+import { projectsReducer } from './ProjectsReducer';
 
 export const rootReducer = combineReducers({
-  currentPalette: currentPaletteReducer(),
-  error: errorReducer(),
-  isLoading: loadingReducer(),
-  projects: projectsReducer()
+  currentPalette: currentPaletteReducer,
+  error: errorReducer,
+  isLoading: loadingReducer,
+  projects: projectsReducer
 });
