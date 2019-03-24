528cef7d4f739475aef2162b0b4019316671754c
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-24 16:41:35 -0600

    Add mockData for testing
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

---
+++ src/mockData/index.js
@@ -0,0 +1,28 @@
+export const mockProjects = [
+  {
+    "id": 1,
+    "name": "Game Time",
+    "created_at": "2019-03-23T18:13:25.614Z",
+    "updated_at": "2019-03-23T18:13:25.614Z"
+  },
+  {
+    "id": 2,
+    "name": "Movie Tracker",
+    "created_at": "2019-03-23T18:13:25.631Z",
+    "updated_at": "2019-03-23T18:13:25.631Z"
+  },
+  {
+    "id": 3,
+    "name": "Trapper Keeper",
+    "created_at": "2019-03-23T18:13:25.652Z",
+    "updated_at": "2019-03-23T18:13:25.652Z"
+  }
+];
+
+export const mockPalette = [
+  { hex: "#FCB97D", isLocked: false },
+  { hex: "#EDD892", isLocked: false },
+  { hex: "#C6B89E", isLocked: false },
+  { hex: "#B5B8A3", isLocked: false },
+  { hex: "#AABA9E", isLocked: false }
+];
