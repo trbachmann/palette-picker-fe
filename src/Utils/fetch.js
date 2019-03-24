8d2d5d46d3de85f8f6d5f55ca7f09376140fdb74
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-24 13:51:32 -0600

    Add fetchData method and createOptions method to Utils
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

---
+++ src/Utils/fetch.js
@@ -0,0 +1,20 @@
+export const fetchData = async (path, options) => {
+    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}api/v1${path}`, options)
+    if(response.status >= 500) {
+      const error = response.body.error.json()
+      throw new Error(error)
+    } else if (response.status >= 300 && response.status < 500) {
+      const error = response.body.json()
+      throw new Error(error)
+    } else {
+      return await response.body.json()
+    }
+};
+
+export const createOptions = (method, data) => ({
+  method,
+  body: JSON.stringify(data),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
