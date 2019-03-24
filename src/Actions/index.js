dc3e6397b6fc360e38ef1df84705e5cdb932af59
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-24 13:33:53 -0600

    Add initial actions
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

---
+++ src/Actions/index.js
@@ -0,0 +1,19 @@
+export const setProjects = (projects) => ({
+  type: 'SET_PROJECTS',
+  projects
+});
+
+export const setError = (error) => ({
+  type: 'SET_ERROR',
+  error
+});
+
+export const toggleLoading = (bool) => ({
+  type: 'TOGGLE_LOADING',
+  isLoading: bool
+});
+
+export const setPaletteColors = (colors) => ({
+  type: 'SET_PALETTE_COLORS',
+  palette: colors
+});
