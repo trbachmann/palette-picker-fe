29db32cf3f69e05208a8e300389426de287cb8c1
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-24 16:43:28 -0600

    Refactor fetchData to handle response with status > 300
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -1,10 +1,7 @@
 export const fetchData = async (path, options) => {
     const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/v1${path}`, options)
-    if(response.status >= 500) {
-      const error = response.json()
-      throw new Error(error)
-    } else if (response.status >= 300 && response.status < 500) {
-      const error = response.json()
+    if(response.status >= 300) {
+      const error = await response.json()
       throw new Error(error)
     } else {
       return await response.json()
