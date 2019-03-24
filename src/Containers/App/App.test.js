f87e29608c930032c692927892ab033ad5f9a832
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-24 16:41:20 -0600

    Add tests for componentDidMount, mapStateToProps and mapDispatchToProps
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from '../../Reducers';
+import { fetchProjects } from '../../Thunks/fetchProjects';
+import * as mockData from '../../mockData';
+
+jest.mock('../../Thunks/fetchProjects');
 
 describe('App', () => {
+  const mockProps = {
+    error: '',
+    isLoading: false,
+    fetchProjects: jest.fn()
+  }
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    const store = createStore(rootReducer);
+    ReactDOM.render(<Provider store={store}><App {...mockProps}/></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   describe('App component', () => {
     let wrapper;
-
+    
     beforeEach(() => {
-      wrapper = shallow(<App/>);
+      wrapper = shallow(<App {...mockProps}/>);
     });
 
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should call fetchProjects on componentDidMount', () => {
+      wrapper.instance().componentDidMount();
+      expect(mockProps.fetchProjects).toHaveBeenCalled();
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with an error and isLoading', () => {
+      const mockState = {
+        error: '',
+        isLoading: false,
+        projects: []
+      }
+      const expected = {
+        error: '',
+        isLoading: false
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with fetchProjects', () => {
+      const mockDispatch = jest.fn();
+      const thunkToDispatch = fetchProjects();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.fetchProjects();
+      expect(mockDispatch).toHaveBeenCalledWith(thunkToDispatch);
     });
   });
 });
