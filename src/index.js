362857baefc61bbbaee150b9e0c41514d9a54a8c
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-24 12:32:29 -0600

    Setup Redux in main index
    Co-authored-by: Tiffany Bachmann <tiffanyrbachmann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './Reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = composeWithDevTools(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
