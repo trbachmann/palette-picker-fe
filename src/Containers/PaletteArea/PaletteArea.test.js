4782a4e57fa47ace756e98fb1d63bd66e446457e
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-25 14:25:24 -0600

    Adds mockData to PaletteArea test for props

--- src/Containers/PaletteArea/PaletteArea.test.js
+++ src/Containers/PaletteArea/PaletteArea.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { PaletteArea } from './PaletteArea';
+import * as mockData from '../../mockData';
 
 describe('PaletteArea', () => {
   describe('PaletteArea component', () => {
     let wrapper;
 
     beforeEach(() => {
-      wrapper = shallow(<PaletteArea/>);
+      wrapper = shallow(<PaletteArea currentPalette={mockData.mockPalette}/>);
     });
 
     it('should match the snapshot', () => {
