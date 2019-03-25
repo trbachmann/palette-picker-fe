f515f49e4e0cc3e39d65accdbdabc859023b0b8f
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-25 15:07:05 -0600

    Add snapshot tetsts for ColorSwatch when color is locked or unlocked

--- src/Components/ColorSwatch/ColorSwatch.test.js
+++ src/Components/ColorSwatch/ColorSwatch.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ColorSwatch from './ColorSwatch';
+import * as mockData from '../../mockData';
 
 describe('ColorSwatch', () => {
   let wrapper;
-
+  const mockColor = mockData.mockPalette[0];
+  const mockLockedColor = {...mockColor, isLocked: true };
   beforeEach(() => {
-    wrapper = shallow(<ColorSwatch/>);
+    wrapper = shallow(<ColorSwatch {...mockColor}/>);
   });
 
-  it('should match the snapshot', () => {
+  it('should match the snapshot when a color is unlocked', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should match the snapshot when a color is locked', () => {
+    wrapper = shallow(<ColorSwatch {...mockLockedColor} />);
+    expect(wrapper).toMatchSnapshot();
+  })
 });
