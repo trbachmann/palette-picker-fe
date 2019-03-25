e83f0ca187dae41f171cdcd416cd4da713b9e45d
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-25 14:38:24 -0600

    Add test for mapStateToProps for ProjectArea

--- src/Containers/ProjectArea/ProjectArea.test.js
+++ src/Containers/ProjectArea/ProjectArea.test.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ProjectArea } from './ProjectArea';
+import * as mockData from '../../mockData'
+import { mapStateToProps } from '../ProjectArea/ProjectArea';
 
 describe('ProjectArea', () => {
   describe('ProjectArea component', () => {
     let wrapper;
+    const mockProps = {
+      projects: mockData.mockProjects
+    }
 
     beforeEach(() => {
-      wrapper = shallow(<ProjectArea/>);
+      wrapper = shallow(<ProjectArea {...mockProps}/>);
     });
 
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot();
     });
   });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with projects', () => {
+      const mockState = {
+        projects: mockData.mockProjects,
+        error: 'Oh no! Error!'
+      }
+      const expected = {
+        projects: mockData.mockProjects,
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
 });
