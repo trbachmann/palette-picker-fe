d55786aba24660018942e4697e2e845537049aa7
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-25 18:40:14 -0600

    Add Tests to Project

--- src/Containers/Project/Project.test.js
+++ src/Containers/Project/Project.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Project } from './Project';
+import { fetchProjectPalettes } from '../../Thunks/fetchProjectPalettes';
+import { mapDispatchToProps } from './Project';
+import { mapStateToProps } from './Project';
+
+jest.mock('../../Thunks/fetchProjectPalettes');
 
 describe('Project', () => {
   describe('Project component', () => {
@@ -19,4 +24,29 @@ describe('Project', () => {
       expect(wrapper).toMatchSnapshot();
     });
   });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with the currentExpandedProject', () => {
+      const mockState = {
+        isLoading: false,
+        error: '', 
+        currentExpandedProject: 1
+      };
+      const expected = {
+        currentExpandedProject: 1
+      };
+      const result = mapStateToProps(mockState);
+      expect(result).toEqual(expected);
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with fetchProjectPalettes', () => {
+      const mockDispatch = jest.fn();
+      const thunkToDispatch = fetchProjectPalettes(mockDispatch);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.fetchProjectPalettes();
+      expect(mockDispatch).toHaveBeenCalledWith(thunkToDispatch);
+    })
+  })
 });
