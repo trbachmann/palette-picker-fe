ac860032f95a8d126ee9f1974005ac4e6b94ed5e
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-26 17:15:37 -0600

    Add more tests for mapDispatchToProps

--- src/Containers/Project/Project.test.js
+++ src/Containers/Project/Project.test.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Project } from './Project';
 import { fetchProjectPalettes } from '../../Thunks/fetchProjectPalettes';
+import { deleteProjectThunk } from '../../Thunks/deleteProjectThunk';
+import { setCurrentExpandedProject } from '../../Actions';
 import { mapDispatchToProps } from './Project';
 import { mapStateToProps } from './Project';
 
 jest.mock('../../Thunks/fetchProjectPalettes');
+jest.mock('../../Thunks/deleteProjectThunk');
 
 describe('Project', () => {
   describe('Project component', () => {
@@ -41,12 +44,31 @@ describe('Project', () => {
   })
 
   describe('mapDispatchToProps', () => {
+    let mockDispatch;
+
+    beforeEach(() => {
+      mockDispatch = jest.fn();
+    });
+
     it('should call dispatch with fetchProjectPalettes', () => {
-      const mockDispatch = jest.fn();
       const thunkToDispatch = fetchProjectPalettes(mockDispatch);
       const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.fetchProjectPalettes();
       expect(mockDispatch).toHaveBeenCalledWith(thunkToDispatch);
     })
-  })
+
+    it('should call dispatch with setCurrentExpandedProject', () => {
+      const actionToDispatch = setCurrentExpandedProject(1);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setCurrentExpandedProject(1);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch with deleteProjectThunk', () => {
+      const thunkToDispatch = deleteProjectThunk(mockDispatch);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.deleteProjectThunk();
+      expect(mockDispatch).toHaveBeenCalledWith(thunkToDispatch);
+    });
+  });
 });
