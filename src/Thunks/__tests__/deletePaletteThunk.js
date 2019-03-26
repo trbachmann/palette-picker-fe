56908b96e71d18da7daa06603f65cdf35d0f7a4b
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-26 13:30:27 -0600

    Update deletePaletteThunk test to account for taking in the projectId

--- src/Thunks/__tests__/deletePaletteThunk.js
+++ src/Thunks/__tests__/deletePaletteThunk.js
@@ -4,8 +4,9 @@ import * as utils from '../../Utils/fetch';
 
 describe('deletePaletteThunk', () => {
   const mockDispatch = jest.fn();
-  const mockId = 2
-  const thunk = deletePaletteThunk(mockId);
+  const mockPaletteId = 2;
+  const mockProjectId = 1;
+  const thunk = deletePaletteThunk(mockPaletteId, mockProjectId);
   const mockIdError = 'No palette exists with id: 0';
 
   beforeEach(() => {
@@ -19,7 +20,7 @@ describe('deletePaletteThunk', () => {
 
   it('should call fetchData with /palettes/:id', async () => {
     const mockOptions = utils.createOptions('DELETE');
-    const mockPath = `/palettes/${mockId}`;
+    const mockPath = `/palettes/${mockPaletteId}`;
     await thunk(mockDispatch);
     expect(utils.fetchData).toHaveBeenCalledWith(mockPath, mockOptions);
   });
@@ -31,7 +32,7 @@ describe('deletePaletteThunk', () => {
 
   it('should call dispatch with deletePalette with an id', async () => {
     await thunk(mockDispatch);
-    expect(mockDispatch).toHaveBeenCalledWith(actions.deletePalette(mockId));
+    expect(mockDispatch).toHaveBeenCalledWith(actions.deletePalette(mockPaletteId, mockProjectId));
   });
 
   it('should call dispatch with toggleLoading with false if no palette with id exists', async () => {
