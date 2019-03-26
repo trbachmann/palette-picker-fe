c8dc83d0852025044addc8f1f03858cd16692313
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-26 15:47:43 -0600

    Update the import for MiniPaletteArea test

--- src/Components/MiniPaletteArea/MiniPaletteArea.test.js
+++ src/Components/MiniPaletteArea/MiniPaletteArea.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MiniPaletteArea } from './MiniPaletteArea';
+import  MiniPaletteArea  from './MiniPaletteArea';
+import * as mockData from '../../mockData';
 
 describe('MiniPaletteArea', () => {
   describe('MiniPaletteArea component', () => {
     let wrapper
-
+    const mockProjectPalettes = mockData.mockProjectPalettes
     beforeEach(() => {
-      wrapper = shallow(<MiniPaletteArea/>);
+      wrapper = shallow(<MiniPaletteArea projectPalettes={mockProjectPalettes}/>);
     });
     
     it('should match the snapshot', () => {
