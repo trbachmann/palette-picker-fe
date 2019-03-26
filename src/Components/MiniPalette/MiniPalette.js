18c3905815014a27789e851a1c95c2736c4a7876
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-26 14:17:02 -0600

    Add colors to mini palette

--- src/Components/MiniPalette/MiniPalette.js
+++ src/Components/MiniPalette/MiniPalette.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const MiniPalette = () => {
+const MiniPalette = (props) => {
+  const {name, color1, color2, color3, color4, color5 } = props
   return(
     <div>
-
+      <p>{name}</p>
+      <div className='MiniPalette--div'>
+        <div className='MiniPalette--div--color' style={{backgroundColor: color1}}></div>
+        <div className='MiniPalette--div--color' style={{backgroundColor: color2}}></div>
+        <div className='MiniPalette--div--color' style={{backgroundColor: color3}}></div>
+        <div className='MiniPalette--div--color' style={{backgroundColor: color4}}></div>
+        <div className='MiniPalette--div--color' style={{backgroundColor: color5}}></div>
+      </div>
     </div>
   )
 }
