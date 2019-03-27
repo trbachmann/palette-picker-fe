43cbf1136e16f73a211e491395a6bd784e87f5bd
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-27 15:04:09 -0600

    Pass down updateIsLocked to ColorSwatch
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

--- src/Containers/PaletteArea/PaletteArea.js
+++ src/Containers/PaletteArea/PaletteArea.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import ColorSwatch from '../../Components/ColorSwatch/ColorSwatch';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { updateIsLocked } from '../../Actions';
 
 export class PaletteArea extends Component{
 
   render() {
-    const colors = this.props.currentPalette.map(color => {
-      return <ColorSwatch {...color} key={color.hex}/>
+    const colors = this.props.currentPalette.map((color, index) => {
+      return <ColorSwatch 
+      {...color} 
+      key={color.hex} 
+      index={index}
+      updateIsLocked={this.props.updateIsLocked}
+      />
     });
 
     return (
@@ -18,11 +24,15 @@ export class PaletteArea extends Component{
   }
 }
 
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   currentPalette: state.currentPalette
 });
 
-export default connect(mapStateToProps)(PaletteArea);
+export const mapDispatchToProps = dispatch => ({
+  updateIsLocked: (index) => dispatch(updateIsLocked(index))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PaletteArea);
 
 PaletteArea.propTypes = {
   currentPalette: PropTypes.array
