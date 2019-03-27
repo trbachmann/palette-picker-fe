b9cb4e33d37fb6d74dc5351402ff6e0b952ab42b
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-27 16:06:59 -0600

    Update ColorSwatch test to account for user clicking the lock icon

--- src/Components/ColorSwatch/ColorSwatch.test.js
+++ src/Components/ColorSwatch/ColorSwatch.test.js
@@ -7,8 +7,11 @@ describe('ColorSwatch', () => {
   let wrapper;
   const mockColor = mockData.mockPalette[0];
   const mockLockedColor = {...mockColor, isLocked: true };
+  const mockIndex = 2;
+  const mockUpdateIsLocked = jest.fn();
+
   beforeEach(() => {
-    wrapper = shallow(<ColorSwatch {...mockColor}/>);
+    wrapper = shallow(<ColorSwatch {...mockColor} index={mockIndex} updateIsLocked={mockUpdateIsLocked}/>);
   });
 
   it('should match the snapshot when a color is unlocked', () => {
@@ -18,5 +21,10 @@ describe('ColorSwatch', () => {
   it('should match the snapshot when a color is locked', () => {
     wrapper = shallow(<ColorSwatch {...mockLockedColor} />);
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+
+  it('should call updateIsLocked when a user clicks the icon', () => {
+    wrapper.find('i').simulate('click');
+    expect(mockUpdateIsLocked).toHaveBeenCalled();
+  });
 });
